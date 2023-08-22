d20945669574738957d83cd4a0fea4c41acee94f
Jeongmin <srm0609@naver.com>
2023-08-22 11:17:54 +0900

    Add: emotion, axios, recoil, svgr, toastify

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
+module.exports = nextConfig;
