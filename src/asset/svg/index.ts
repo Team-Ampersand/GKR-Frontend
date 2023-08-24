1f2f6ce14008cd508ca7ce7883130ffcd5cd55a5
Jeongmin <srm0609@naver.com>
2023-08-24 16:12:52 +0900

    Add: svg

--- src/asset/svg/index.ts
+++ src/asset/svg/index.ts
@@ -5,5 +5,17 @@ import Profile from 'asset/svg/Profile.svg';
 import Handshake from 'asset/svg/HandShake.svg';
 import Bell from 'asset/svg/Bell.svg';
 import Exit from 'asset/svg/Exit.svg';
+import GKRLogoA from 'asset/svg/GKRLogoA.svg';
+import Document from 'asset/svg/Document.svg';
 
-export { GauthLgo, GKRLogo, Folder, Profile, Handshake, Bell, Exit };
+export {
+  GauthLgo,
+  GKRLogo,
+  Folder,
+  Profile,
+  Handshake,
+  Bell,
+  Exit,
+  GKRLogoA,
+  Document,
+};
