253f9f672d1ed13fde70a97300293bd7b85055d9
kangkong <kang3530554@gmail.com>
2023-08-30 17:39:25 +0900

    💬 Update: lib path

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ import RentalList from 'components/Home/organisms/RentalList';
 import { HomeTemplate } from 'components/Home/template/style';
 import SearchField from 'components/common/molecules/SearchField';
 import Sidebar from 'components/common/organisms/Sidebar';
-import { CommonTemplate } from 'components/common/templates/CommonTemplate/style';
+import { CommonTemplate } from 'components/common/templates/style';
 
 const HomePage = () => {
   return (
