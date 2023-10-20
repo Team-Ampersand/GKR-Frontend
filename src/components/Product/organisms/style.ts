6aa252cbc661f6cfa2edd7610735a836b531f45c
한준 <qw2661807@gmail.com>
2023-10-21 00:12:56 +0900

    :fire: Remove Banner Background Code

--- src/components/Product/organisms/style.ts
+++ src/components/Product/organisms/style.ts
@@ -1,23 +1,11 @@
-import styled from '@emotion/styled';
-import { Palette } from 'style/global';
+import styled from '@emotion/styled'
+import { Palette } from 'style/global'
 
 export const Wrapper = styled.div`
-  width: 800px;
-  padding: 40px;
-  border-radius: 10px;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  background: ${Palette.NATURAL_N2};
-
-  p {
-    font-size: 32px;
-    font-weight: 900;
-    color: ${Palette.PRIMARY_P5};
-    padding-bottom: 32px;
-  }
-`;
+`
 
 export const BtnBox = styled.div`
   width: 320px;
-`;
+`
