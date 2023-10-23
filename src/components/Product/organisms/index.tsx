80849680c1bebaabf474396e0750134eda5c226a
한준 <qw2661807@gmail.com>
2023-10-23 18:19:15 +0900

    :art: Improve structure
    물품 등록하기와 삭제하기 컴포넌트 구별

--- src/components/Product/organisms/index.tsx
+++ src/components/Product/organisms/index.tsx
@@ -1,22 +1,41 @@
+import { ProductChoice } from 'recoilAtoms'
 import Button from '../../common/atoms/Button'
-import ImgBox from '../molecules/ImgBox'
 import ChoiceBox from '../molecules/ChoiceBox'
 import InputBox from '../molecules/InputBox'
 import ListBox from '../molecules/ListBox'
+import ImgBox from '../molecules/\bImgBox'
 import * as S from './style'
+import { useRecoilValue } from 'recoil'
 
 const Regist = () => {
+  const choice = useRecoilValue(ProductChoice)
+
+  const renderForm = () => {
+    switch (choice) {
+      case '물품 등록하기':
+        return (
+          <>
+            <ChoiceBox />
+            <ListBox />
+            <InputBox />
+            <ImgBox />
+          </>
+        )
+      case '물품 삭제하기':
+        return <ChoiceBox /> 
+      default:
+        return null
+    }
+  }
+
   return (
     <S.Wrapper>
-      <S.TopBox>
-        <ChoiceBox />
-        <ListBox />
-        <InputBox />
-        <ImgBox />
-      </S.TopBox>
-      <S.BtnBox>
-        <Button text="등록하기" fontweight="700" />
-      </S.BtnBox>
+      <S.TopBox>{renderForm()}</S.TopBox>
+      {choice === '물품 등록하기' && (
+        <S.BtnBox>
+          <Button text="등록하기" fontweight="700" />
+        </S.BtnBox>
+      )}
     </S.Wrapper>
   )
 }
