08261090d3a883d94c256730873c6ca9a3b8c53e
한준 <qw2661807@gmail.com>
2023-10-23 23:19:09 +0900

    :bento: Update assests Camel
    svg 카멜로 변경

--- src/asset/svg/Exit.tsx
+++ src/asset/svg/Exit.tsx
@@ -1,9 +1,25 @@
 export default function Exit() {
-    return (
-        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M5.42188 11.0007H21M21 11.0007L17.8843 8.27344M21 11.0007L17.8843 13.728" stroke="#999999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M13.9817 6.78947V3.28571C13.9817 2.02335 12.9584 1 11.696 1H3.28571C2.02335 1 1 2.02335 1 3.28571V18.7143C1 19.9767 2.02335 21 3.28571 21H11.696C12.9584 21 13.9817 19.9767 13.9817 18.7143V15.7368" stroke="#999999" stroke-width="2" stroke-linecap="round" />
-        </svg>
-
-    )
+  return (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.42188 11.0007H21M21 11.0007L17.8843 8.27344M21 11.0007L17.8843 13.728"
+        stroke="#999999"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13.9817 6.78947V3.28571C13.9817 2.02335 12.9584 1 11.696 1H3.28571C2.02335 1 1 2.02335 1 3.28571V18.7143C1 19.9767 2.02335 21 3.28571 21H11.696C12.9584 21 13.9817 19.9767 13.9817 18.7143V15.7368"
+        stroke="#999999"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  )
 }
