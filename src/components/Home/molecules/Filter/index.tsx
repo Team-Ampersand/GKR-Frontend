74584fadf0a85f5b637869eeb24d8315e15267d4
한준 <qw2661807@gmail.com>
2023-10-23 20:50:12 +0900

    :art: Change Function Name

--- src/components/Home/molecules/Filter/index.tsx
+++ src/components/Home/molecules/Filter/index.tsx
@@ -1,10 +1,10 @@
-import MenuButton from 'components/Home/atoms/Button/MenuButton';
-import { FilterListData } from "asset/data/FilterListData"
-import { useState } from 'react';
-import * as S from "./style";
-import FilterModal from 'components/Home/atoms/Modal/FilterModal';
-import FilterButton from 'components/Home/atoms/Button/FilterButton';
-function MenuList() {
+import MenuButton from 'components/Home/atoms/Button/MenuButton'
+import { FilterListData } from 'asset/data/FilterListData'
+import { useState } from 'react'
+import * as S from './style'
+import FilterModal from 'components/Home/atoms/Modal/FilterModal'
+import FilterButton from 'components/Home/atoms/Button/FilterButton'
+function Filter() {
   const [type, setType] = useState(FilterListData.equipmentType[0].name)
   const [state, setState] = useState(FilterListData.equipmentStatusList[0].name)
   const [modal, setModal] = useState(false)
@@ -15,7 +15,7 @@ function MenuList() {
         {modal ? <FilterModal /> : null}
       </S.FilterModalWrapper>
     </S.FilterListWrapper>
-  );
-};
+  )
+}
 
-export default MenuList;
+export default Filter
