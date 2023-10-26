d4280dd7fd1af51a02fc8258fcf946a0c7ec361f
한준 <qw2661807@gmail.com>
2023-10-26 22:34:57 +0900

    :lipstick: Add Voilation List File

--- src/components/Violation/organisms/index.tsx
+++ src/components/Violation/organisms/index.tsx
@@ -1,10 +1,12 @@
 import TopBox from '../molecules/Box/TopBox'
+import ViolationList from '../molecules/Box/VoilationListBox'
 import * as S from './style'
 
 const Violation = () => {
   return (
     <S.Wrapper>
       <TopBox />
+      <ViolationList />
     </S.Wrapper>
   )
 }
