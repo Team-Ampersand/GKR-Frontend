d373750e144d281742f7614d31992c57775a288f
한준 <qw2661807@gmail.com>
2023-10-30 22:49:29 +0900

    :sparkles: Add LocalDateTime Change Function
    시간 변경 함수 생성

--- src/components/common/atoms/NoticeItem/index.tsx
+++ src/components/common/atoms/NoticeItem/index.tsx
@@ -1,17 +1,35 @@
 interface NoticeItemPropsType {
   title: string
-  id: number
-  roleType?: 'member' | 'admin'
+  id?: number
   createNoticeDate: string
+  content: string
+}
+
+import * as S from './style'
+
+function formatDate(inputDate: string) {
+  const parsedDate = new Date(inputDate)
+  const year = parsedDate.getFullYear()
+  const month = (parsedDate.getMonth() + 1).toString().padStart(2, '0')
+  const day = parsedDate.getDate().toString().padStart(2, '0')
+  return `${year}.${month}.${day}`
 }
 
 const NoticeItem = ({
   title,
-  id,
-  roleType,
+  content,
   createNoticeDate,
 }: NoticeItemPropsType) => {
-  return <div>NoticeItem</div>
+  const formattedDate = formatDate(createNoticeDate)
+  return (
+    <S.NoticeItemWrapper>
+      <S.Top>
+        <S.Title>[{title}]</S.Title>
+        <S.Date>{formattedDate}</S.Date>
+      </S.Top>
+      <S.Content>{content}</S.Content>
+    </S.NoticeItemWrapper>
+  )
 }
 
 export default NoticeItem
