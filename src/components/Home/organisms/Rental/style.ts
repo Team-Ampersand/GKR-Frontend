ced7238223b1c09a2f181e059bc9a431d90d5703
이아론 <aron0613@naver.com>
2023-11-01 18:20:45 +0900

    :bug: Fix RentalList Scrollbar
    rentalList의 요소가 전부 보이지 않는 문제가 발생해 수정했습니다.

--- src/components/Home/organisms/Rental/style.ts
+++ src/components/Home/organisms/Rental/style.ts
@@ -1,8 +1,18 @@
 import styled from '@emotion/styled'
+import { Palette } from 'style/global'
 
 export const RentalWrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  overflow-y: auto;
+  ::-webkit-scrollbar {
+    width: 6px;
+    background: ${Palette.NATURAL_N2};
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${Palette.NATURAL_N4};
+    border-radius: 10px;
+  }
 `
 
 export const LineWrapper = styled.div`
@@ -10,4 +20,5 @@ export const LineWrapper = styled.div`
   height: 2px;
   background-color: #c2c2c2;
   margin-bottom: 0.5rem;
+  
 `
