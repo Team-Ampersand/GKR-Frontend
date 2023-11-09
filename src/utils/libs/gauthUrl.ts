5da932cf1b49406b1afd2e98b0aa70616abf1e84
한준 <qw2661807@gmail.com>
2023-11-09 23:27:39 +0900

    :zap: Update gauthURl

--- src/utils/libs/gauthUrl.ts
+++ src/utils/libs/gauthUrl.ts
@@ -1,3 +1,1 @@
-import { clientId, redirectUri } from './env'
-
-export const gauthUrl = `https://gauth.co.kr/login?client_id=${clientId}&redirect_uri=${redirectUri}`
+export const gauthUrl = `https://gauth.co.kr/login?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`
