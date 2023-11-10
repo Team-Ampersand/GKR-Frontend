29605f687472e77d007bb7145b970ebac3079ad5
한준 <qw2661807@gmail.com>
2023-11-10 15:57:58 +0900

    :recycle: Refactor Cookie maxAge

--- src/utils/libs/setToken.ts
+++ src/utils/libs/setToken.ts
@@ -9,9 +9,9 @@ export const setToken = (
   setCookie(ctx, 'AccessToken', `Bearer ${AccessTokenn}`, {
     maxAge: 10800,
     path: '/',
-  })
+  }) // 3시간
   setCookie(ctx, 'RefreshToken', `Bearer ${RefreshToken}`, {
-    maxAge: 15768000,
+    maxAge: 604800,
     path: '/',
-  })
+  }) // 1주일
 }
