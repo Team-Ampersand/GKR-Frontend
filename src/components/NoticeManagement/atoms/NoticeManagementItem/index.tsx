a4b7fd6d5116d19c4fe6f54c0caf8e3ab67d13df
이아론 <aron0613@naver.com>
2023-11-11 00:30:14 +0900

    :lipstick: Add NoticeManagement component

---
+++ src/components/NoticeManagement/atoms/NoticeManagementItem/index.tsx
@@ -0,0 +1,35 @@
+import { NoticeManagementItemPropsType } from 'types/components/NoticeManagement/NoticManagementType'
+import * as S from './style'
+import * as I from 'asset/svg'
+import Link from 'next/link'
+export default function NoticeManagementItem({
+  id,
+  title,
+  content,
+  imageUrl,
+  createNoticeDate,
+}: NoticeManagementItemPropsType) {
+  return (
+    <S.ViolationItemWrapper>
+      <Link href={`/notice/${id}`}>
+        <S.ContentWrapper>
+          <S.TitleWrapper>
+            <S.Title>
+              [<S.TitleContent>{title}</S.TitleContent>]
+            </S.Title>
+            <S.Date>{createNoticeDate}</S.Date>
+          </S.TitleWrapper>
+          <S.Content>{content}</S.Content>
+        </S.ContentWrapper>
+      </Link>
+      <S.ButtonListWrapper>
+        <S.ButtonWrapper onClick={() => {}}>
+          <I.Edit_Outline stroke="#000000" width="24px" height="24px" />
+        </S.ButtonWrapper>
+        <S.ButtonWrapper onClick={() => {}}>
+          <I.Delete strok="#000000" width="24px" height="24px" />
+        </S.ButtonWrapper>
+      </S.ButtonListWrapper>
+    </S.ViolationItemWrapper>
+  )
+}
