8fb5924328e84f66b0cc5991fb721de2403bf418
이아론 <aron0613@naver.com>
2023-11-11 00:49:20 +0900

    :lipstick: Add admin router

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -3,14 +3,23 @@ import Sidebar from 'components/common/organisms/Sidebar'
 import { CommonTemplate } from 'components/common/templates/style'
 import Notice from 'components/Notice/organisms/Notice'
 import { NoticeTemplate } from 'components/Notice/templates/style'
-
+import NoticeMangement from 'components/NoticeManagement/organisms/NoticeManagement'
+import { NoticeManagementTemplate } from 'components/NoticeManagement/templates/style'
+import IsAdmin from 'utils/IsAdmin'
 const NoticePage = () => {
+  const isAdmin = IsAdmin()
   return (
     <CommonTemplate>
       <Sidebar />
-      <NoticeTemplate>
-        <Notice />
-      </NoticeTemplate>
+      {isAdmin ? (
+        <NoticeManagementTemplate>
+          <NoticeMangement />
+        </NoticeManagementTemplate>
+      ) : (
+        <NoticeTemplate>
+          <Notice />
+        </NoticeTemplate>
+      )}
     </CommonTemplate>
   )
 }
