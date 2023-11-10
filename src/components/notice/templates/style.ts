fd85c5e8679ed1f49ba91f55b41055eae924b55c
이아론 <aron0613@naver.com>
2023-11-11 00:49:50 +0900

    :lipstick: Update noticeTemplate style

--- src/components/notice/templates/style.ts
+++ src/components/notice/templates/style.ts
@@ -3,6 +3,10 @@ import { MainTemplates } from 'components/common/templates/style'
 import { Palette } from 'style/global'
 
 export const NoticeTemplate = styled(MainTemplates)`
-  background-color: ${Palette.BACKGROUND_BG};
+  width: calc(100% - 250px);
+  height: 100dvh;
+  display: flex;
+  flex-direction: column;
   padding: 24px 80px;
+  overflow-y: scroll;
 `
