662364e40408fa5e31f3b7a2383f53775b7d7e0c
한준 <qw2661807@gmail.com>
2023-11-11 00:22:08 +0900

    :sparkles: Make a request according to params
    params에 따라 요청가도록 로직 구현

--- src/utils/apis/equipment.ts
+++ src/utils/apis/equipment.ts
@@ -1,11 +1,29 @@
 import { toast } from 'react-toastify'
 import { apiClient } from 'utils/libs/apiClient'
-import { EquipmentController } from 'utils/libs/requestUrls'
 
-export const getEquipment = async () => {
+type paramsObj = {
+  equipmentStatus?: string
+  equipmentType?: string
+}
+
+export const getEquipment = async (url: string, params?: paramsObj) => {
   try {
-    const url = EquipmentController.getEquipment()
-    const { data } = await apiClient.get(url)
+    let config = {}
+
+    if (params && params.equipmentStatus) {
+      config = {
+        params: {
+          equipmentStatus: params.equipmentStatus,
+        },
+      }
+    } else if (params && params.equipmentType) {
+      config = {
+        params: {
+          equipmentType: params.equipmentType,
+        },
+      }
+    }
+    const { data } = await apiClient.get(url, config)
     return { data }
   } catch (error) {
     toast.error('알수없는 에러가 발생했습니다.')
