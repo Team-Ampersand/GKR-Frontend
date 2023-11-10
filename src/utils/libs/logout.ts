e8efa06d456bdc95f757136c1aba4ca7ae73f5a7
한준 <qw2661807@gmail.com>
2023-11-10 10:46:40 +0900

    :art: Add ToastOption

--- src/utils/libs/logout.ts
+++ src/utils/libs/logout.ts
@@ -1,10 +1,10 @@
 import { mutate } from 'swr'
 import { removeToken } from './removeToken'
 import { toast } from 'react-toastify'
+import toastOption from 'utils/libs/toastOption'
 
 export const logout = () => {
   removeToken()
   mutate(() => true, undefined, { revalidate: false })
-  removeToken()
-  toast.success('로그아웃 성공')
+  toast.success('로그아웃되었습니다.', toastOption)
 }
