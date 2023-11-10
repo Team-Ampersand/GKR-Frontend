0b3435a07b4c1f7f4c08c41e83842ab0c325b44e
한준 <qw2661807@gmail.com>
2023-11-10 22:28:48 +0900

    :memo: Add Config Json

--- src/utils/libs/apiClient.ts
+++ src/utils/libs/apiClient.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 import { getRefresh } from './getRefresh'
+import BASE_HEADER from '../Config/Config.json'
+
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
   withCredentials: true,
+  headers: BASE_HEADER,
 })
 apiClient.interceptors.request.use(getRefresh)
