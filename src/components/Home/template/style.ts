916c2c5273a5be1b93285df0ba8dd333ea1c0570
이아론 <aron0613@naver.com>
2023-11-11 22:54:09 +0900

    :bug: Fix rentalList Scrollbar Issues

--- src/components/Home/template/style.ts
+++ src/components/Home/template/style.ts
@@ -1,9 +1,8 @@
-import styled from '@emotion/styled';
-import { MainTemplates } from 'components/common/templates/style';
-import { Palette } from 'style/global';
+import styled from '@emotion/styled'
+import { MainTemplates } from 'components/common/templates/style'
+import { Palette } from 'style/global'
 
 export const HomeTemplate = styled(MainTemplates)`
   background-color: ${Palette.BACKGROUND_BG};
   padding: 24px 80px;
-  overflow: hidden;
-`;
+`
