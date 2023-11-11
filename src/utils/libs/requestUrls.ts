21d7dfc585cbf3d8a3736c188fe4c2bdeabc61b4
한준 <qw2661807@gmail.com>
2023-11-11 22:17:28 +0900

    :sparkles: Add requestUrl
    상세보기 가져오는 url 추가

--- src/utils/libs/requestUrls.ts
+++ src/utils/libs/requestUrls.ts
@@ -1,4 +1,5 @@
 export const EquipmentController = {
   getEquipment: (filter?: string) =>
     filter ? `/equipment/${filter}` : '/equipment',
+  getDetail: (id: string | string[]) => `/equipment/${id}`,
 }
