1caa856a954fa97782764f79c004fae57cc2d1eb
한준 <qw2661807@gmail.com>
2023-11-12 21:21:13 +0900

    :art: Edit Date

--- src/components/ViolationMenagement/atoms/ViolationItem/index.tsx
+++ src/components/ViolationMenagement/atoms/ViolationItem/index.tsx
@@ -1,19 +1,28 @@
 import { ViolationItemPropsType } from 'types/components/ViolationManagement/ViolationType'
 import * as S from './style'
 import * as I from 'asset/svg'
+const formatDate = (dateString: string): string => {
+  const date = new Date(dateString)
+  const year = date.getFullYear().toString().slice(2)
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const day = date.getDate().toString().padStart(2, '0')
+  return `${year}.${month}.${day}`
+}
 export default function ViolationItem({
   userName,
   violationReason,
   violationStartDate,
   violationEndDate,
 }: ViolationItemPropsType) {
+  const formattedStartDate = formatDate(violationStartDate)
+  const formattedEndDate = formatDate(violationEndDate)
   return (
     <S.ViolationItemWrapper>
       <S.ContentWrapper>
         <S.TitleWrapper>
           <S.Title>[제재사항]</S.Title>
           <S.Username>{userName}</S.Username>
-          <S.Date>{violationStartDate + '~' + violationEndDate}</S.Date>
+          <S.Date>{formattedStartDate + '~' + formattedEndDate}</S.Date>
         </S.TitleWrapper>
         <S.Content>{violationReason}</S.Content>
       </S.ContentWrapper>
