18f834f5fe1400d2205a82b716e374712c1e6c51
한준 <qw2661807@gmail.com>
2023-11-13 20:42:04 +0900

    :art: Update filename

---
+++ src/components/Rental/organisms/UserRental/index.tsx
@@ -0,0 +1,13 @@
+import TopBox from 'components/common/molecules/TopBox'
+import * as S from './style'
+import RentalList from 'components/Rental/molecules/RentalList'
+
+const UserRental = () => {
+  return (
+    <S.Wrapper>
+      <TopBox text="대여 물품" />
+    </S.Wrapper>
+  )
+}
+
+export default UserRental
