8b51220117f3fbe67c07d6afa758ad8ac6ead373
이아론 <aron0613@naver.com>
2023-11-14 15:41:53 +0900

    :pencil2: Fix deleteNotice requsetUrl props Type

--- src/utils/libs/requestUrls.ts
+++ src/utils/libs/requestUrls.ts
@@ -27,5 +27,5 @@ export const NoticeController = {
   getNoticeDetail: (id: string | string[]) => `/notice/${id}`,
   postNoticeWrite: () => `/notice/create`,
   editNotice: (id: string) => `/notice/edit/${id}`,
-  deleteNotice: (id: string) => `/notice/delete/${id}`,
+  deleteNotice: (id: number) => `/notice/delete/${id}`,
 }
