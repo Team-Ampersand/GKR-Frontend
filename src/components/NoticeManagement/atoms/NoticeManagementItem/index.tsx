4063a43a2a0d578a9fb6241aca8365afd53ef637
이아론 <aron0613@naver.com>
2023-11-14 15:42:11 +0900

    :sparkles: Add delete Notice

--- src/components/NoticeManagement/atoms/NoticeManagementItem/index.tsx
+++ src/components/NoticeManagement/atoms/NoticeManagementItem/index.tsx
@@ -2,6 +2,10 @@ import { NoticeManagementItemPropsType } from 'types/components/NoticeManagement
 import * as S from './style'
 import * as I from 'asset/svg'
 import Link from 'next/link'
+import { useMutation } from 'react-query'
+import { NoticeController } from 'utils/libs/requestUrls'
+import { deleteData } from 'utils/apis/data'
+import { toast } from 'react-toastify'
 export default function NoticeManagementItem({
   id,
   title,
@@ -10,6 +14,23 @@ export default function NoticeManagementItem({
   createNoticeDate,
 }: NoticeManagementItemPropsType) {
   const date = createNoticeDate.split('T')[0].replace(/-/g, '.')
+  const url = NoticeController.deleteNotice(id)
+  const { data, mutate } = useMutation(
+    ['notice', url],
+    () => {
+      return deleteData(url)
+    },
+    {
+      onSuccess: () => {
+        toast.success('삭제되었습니다')
+      },
+    },
+  )
+
+  const deleteNotice = () => {
+    mutate()
+  }
+
   return (
     <S.ViolationItemWrapper>
       <Link href={`/notice/${id}`}>
@@ -27,7 +48,7 @@ export default function NoticeManagementItem({
         <S.ButtonWrapper onClick={() => {}}>
           <I.Edit_Outline stroke="#000000" width="24px" height="24px" />
         </S.ButtonWrapper>
-        <S.ButtonWrapper onClick={() => {}}>
+        <S.ButtonWrapper onClick={deleteNotice}>
           <I.Delete strok="#000000" width="24px" height="24px" />
         </S.ButtonWrapper>
       </S.ButtonListWrapper>
