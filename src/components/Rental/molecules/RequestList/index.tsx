49c28da99dfee76047919632d8188016285cfade
한준 <qw2661807@gmail.com>
2023-11-16 15:18:44 +0900

    :recycle: Improve map props

--- src/components/Rental/molecules/RequestList/index.tsx
+++ src/components/Rental/molecules/RequestList/index.tsx
@@ -33,15 +33,15 @@ const RequestList = () => {
   return (
     <S.RentalListWrapper>
       {orderEquipmentList &&
-        [...orderEquipmentList].map((equipment) => (
+        [...orderEquipmentList].map((orderEquipment) => (
           <RentalItem
-            key={equipment.id}
-            id={equipment.id}
-            title={equipment.name}
-            imageUrl={equipment.imageUrl}
-            description={equipment.description}
-            equipmentStatus={equipment.equipmentStatus}
-            equipmentType={equipment.equipmentType}
+            key={orderEquipment.id}
+            id={orderEquipment.id}
+            title={orderEquipment.name}
+            imageUrl={orderEquipment.imageUrl}
+            description={orderEquipment.description}
+            equipmentStatus={orderEquipment.equipmentStatus}
+            equipmentType={orderEquipment.equipmentType}
             role={role}
           />
         ))}
