a75bd210bfe450a0117b0a652a187b7b755a2eeb
한준 <qw2661807@gmail.com>
2023-11-20 20:13:45 +0900

    :recycle: Edit searchField Input value

--- src/components/common/molecules/SearchField/index.tsx
+++ src/components/common/molecules/SearchField/index.tsx
@@ -1,36 +1,32 @@
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useState, useEffect } from 'react'
 import * as S from './style'
 import { useRecoilValue } from 'recoil'
 import { roleType } from 'recoilAtoms'
-import * as I from 'asset/svg'
 
 const SearchField = () => {
-  const [timer, setTimer] = useState<NodeJS.Timeout>()
-  const { register, getValues } = useForm<{ text: string }>({
-    defaultValues: { text: '' },
-  })
-
   const role = useRecoilValue(roleType)
+  const [inputValue, setInputValue] = useState('')
 
-  const onChange = (e: string) => {
-    clearTimeout(timer)
+  useEffect(() => {
+    
+  }, [inputValue])
 
-    const a = setTimeout(() => {
-      console.log(getValues('text'))
-    }, 500)
-
-    setTimer(a)
+  const handleInputChange = (e: any) => {
+    setInputValue(e.target.value)
   }
 
   return (
-    <S.Label
-      {...register('text', {
-        onChange: onChange,
-      })}
-    >
-      <S.Input role={role} type="text" placeholder="검색어를 입력해주세요" />
-    </S.Label>
+    <>
+      <S.Label>
+        <S.Input
+          role={role}
+          type="text"
+          placeholder="검색어를 입력해주세요"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </S.Label>
+    </>
   )
 }
 
