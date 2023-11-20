0cc9042e3b0aa73b251e6c6d3da2a055f4a2fb88
한준 <qw2661807@gmail.com>
2023-11-20 19:53:36 +0900

    :sparkles: Add search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+import Rental from 'components/Home/organisms/Rental'
+import { HomeTemplate } from 'components/Home/template/style'
+import SearchField from 'components/common/molecules/SearchField'
+import Sidebar from 'components/common/organisms/Sidebar'
+import { CommonTemplate } from 'components/common/templates/style'
+
+const HomePage = () => {
+  return (
+    <CommonTemplate>
+      <Sidebar />
+      <HomeTemplate>
+        <SearchField />
+        <Rental />
+      </HomeTemplate>
+    </CommonTemplate>
+  )
+}
+export default HomePage
