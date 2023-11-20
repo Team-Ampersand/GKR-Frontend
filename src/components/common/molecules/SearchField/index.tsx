cf0d3ef6b81f7c2dbd716a01767b23c0195e9584
한준 <qw2661807@gmail.com>
2023-11-20 20:38:24 +0900

    :sparkles: Add searchList fetcher

--- src/components/common/molecules/SearchField/index.tsx
+++ src/components/common/molecules/SearchField/index.tsx
@@ -2,18 +2,34 @@ import { useState, useEffect } from 'react'
 import * as S from './style'
 import { useRecoilValue } from 'recoil'
 import { roleType } from 'recoilAtoms'
+import { useQuery } from 'react-query'
+import { EquipmentController } from 'utils/libs/requestUrls'
+import { getData } from 'utils/apis/data'
 
 const SearchField = () => {
   const role = useRecoilValue(roleType)
   const [inputValue, setInputValue] = useState('')
-
+  const url = EquipmentController.serachEquipment()
+  const { data, refetch } = useQuery(
+    ['search', url],
+    () => {
+      const queryParams = { name: inputValue }
+      return getData(url, queryParams)
+    },
+    {
+      enabled: !!url,
+      refetchOnWindowFocus: false,
+    },
+  )
+  const searchList = data?.data?.equipmentList
+  console.log(searchList)
   useEffect(() => {
     const timer = setTimeout(() => {
-      console.log('Delayed search:', inputValue)
+      refetch()
     }, 500)
 
     return () => clearTimeout(timer)
-  }, [inputValue])
+  }, [inputValue, refetch])
 
   const handleInputChange = (e: any) => {
     setInputValue(e.target.value)
