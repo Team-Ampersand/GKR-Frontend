58f167b41c1e95f1de444e8c9448ddfbf361c739
이아론 <aron0613@naver.com>
2023-12-18 20:47:43 +0900

    🚚: Rename notice folder

---
+++ src/components/Notice/organisms/NoticeDetail/index.tsx
@@ -0,0 +1,42 @@
+import { NoticeDetailDummy } from 'asset/dummy/NoticeDetailDummy'
+import DetailBox from 'components/common/molecules/DetailBox'
+import { useState, useEffect } from 'react'
+import { NoticeController } from 'utils/libs/requestUrls'
+import { useQuery } from 'react-query'
+import * as S from './style'
+import { useParams } from 'next/navigation'
+import { getData } from 'utils/apis/data'
+
+const NoticeDetail = () => {
+  const params = useParams()
+  const url = NoticeController.getNoticeDetail(params.id)
+  const { data, refetch } = useQuery(
+    ['noticeList', url],
+    () => {
+      return getData(url)
+    },
+    {
+      enabled: !!url,
+      refetchOnWindowFocus: false,
+    },
+  )
+  console.log(data)
+  useEffect(() => {
+    refetch()
+  }, [refetch])
+  return (
+    <S.Wrapper>
+      {data ? (
+        <DetailBox
+          id={data.data.id}
+          title={data.data.title}
+          createNoticeDate={'2022.06.26 - 06:23'}
+          content={data.data.content}
+          imageUrl={data.data.imageUrl}
+        />
+      ) : null}
+    </S.Wrapper>
+  )
+}
+
+export default NoticeDetail
