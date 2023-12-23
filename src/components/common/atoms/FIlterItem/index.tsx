fdb755523495352ecc0a0a944fe5ce68d1b30382
이아론 <aron0613@naver.com>
2023-12-24 04:47:18 +0900

    Fix filter duplication problem

--- src/components/common/atoms/FIlterItem/index.tsx
+++ src/components/common/atoms/FIlterItem/index.tsx
@@ -1,19 +1,21 @@
-import { FilterItemPropsType } from "types/components/Home/FilterTypes"
-import * as S from "./style"
-import { useRecoilState } from "recoil"
-import { filterState, roleType } from "recoilAtoms"
+import { FilterItemPropsType } from 'types/components/Home/FilterTypes'
+import * as S from './style'
+import { useRecoilState } from 'recoil'
+import { filterState, roleType } from 'recoilAtoms'
 export default function FilterItem({ name, value, type }: FilterItemPropsType) {
-    const [state, setState] = useRecoilState(filterState)
-    const [role, setRole] = useRecoilState(roleType)
-    const select = type ? (state.equipmentType === value) : (state.equipmentStatus === value)
-    const onClick = () => {
-        type ?
-            setState({ ...state, equipmentType: value })
-            : setState({ ...state, equipmentStatus: value })
-    }
-    return (
-        <S.FilterItemWrapper onClick={onClick} value={select} role={role} >
-            {name}
-        </S.FilterItemWrapper>
-    )
+  const [state, setState] = useRecoilState(filterState)
+  const [role, setRole] = useRecoilState(roleType)
+  const select = type
+    ? state.equipmentType === value
+    : state.equipmentStatus === value
+  const onClick = () => {
+    type
+      ? setState({ ...state, equipmentStatus: '', equipmentType: value })
+      : setState({ ...state, equipmentStatus: value, equipmentType: '' })
+  }
+  return (
+    <S.FilterItemWrapper onClick={onClick} value={select} role={role}>
+      {name}
+    </S.FilterItemWrapper>
+  )
 }
