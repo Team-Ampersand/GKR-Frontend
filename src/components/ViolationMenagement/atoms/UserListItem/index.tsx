2046e7d2944d9e152bc08c334ca8de7fc78f274e
이아론 <aron0613@naver.com>
2024-03-18 18:09:34 +0900

    Add word converter

--- src/components/ViolationMenagement/atoms/UserListItem/index.tsx
+++ src/components/ViolationMenagement/atoms/UserListItem/index.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 import * as S from './style'
 import * as I from 'asset/svg'
 import { Dispatch, SetStateAction } from 'react'
-import UserTypes from 'types/components/common/UserType'
+import UserTypes, { UserRoleType } from 'types/components/common/UserType'
+import roleListData from 'asset/data/roleListData'
 
 interface UserListProps extends UserTypes {
   violationEmail: string
@@ -21,6 +22,10 @@ const UserListItem = ({
   setViolationEmail,
 }: UserListProps) => {
   const isFocus = violationEmail === email
+
+  const transformWard = (ward: UserRoleType): string => {
+    return roleListData[ward]
+  }
   return (
     <S.Wrapper
       onClick={() => setViolationEmail(isFocus ? '' : email)}
@@ -30,9 +35,13 @@ const UserListItem = ({
         <I.Profile />
         <p>{name}</p>
       </div>
-      <p>{[grade, classNum, stuNum].map((i) => i.toString()).join('')}</p>
+      <p>
+        {[grade, classNum, stuNum.toString().padStart(2, '0')]
+          .map((i) => i.toString())
+          .join('')}
+      </p>
       <p>{rentalRestricted ? '대여정지' : '정상'}</p>
-      <p>{role}</p>
+      <p>{transformWard(role)}</p>
     </S.Wrapper>
   )
 }
